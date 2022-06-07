6389cf1b73dc6b08f52960cba69316b2435e2068
Msmamatha <106484965+Msmamatha@users.noreply.github.com>
2022-06-07 16:16:42 +0530

    Add files via upload

---
+++ Simulation.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+
+namespace WirelessNodeSimulation
+{
+	
+	public class Simulation
+	{
+	
+		public ArrayList Calls = new ArrayList();
+		public ArrayList NetworkUtilisation = new ArrayList();
+		public ArrayList AnnoText=new ArrayList();
+	}
+}
